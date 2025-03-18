b170c680889396008a337b221224c9acd246a86e
vivian <vivian.njoroge@ramani.co.ke>
2025-03-18 15:54:27 +0300

    Implement delete functionality

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
-import './assets/main.css'
-import 'primeicons/primeicons.css'
-import Toast from 'vue-toastification/index.css'
+import './assets/main.css';
+import 'primeicons/primeicons.css';
+import Toast from 'vue-toastification';
+import 'vue-toastification/dist/index.css';
 import router from './router';
 
 
